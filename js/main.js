d5a475054773275a46a6e8c050f3524b399dd299
Dmitrii Suvorov <sdk503@yandex.ru>
2022-02-22 22:26:27 +0700

    add randomization functions (2.15)

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function getRandomInt(a,b){
+  const diff = b-a;
+  if(diff>=0){
+    return a + Math.round(Math.random() * diff);
+  }else{
+    return Error("getRandomInt: Ошибка входных данных");
+  }
+}
+
+function getRandomFloat(a,b,accuracy){
+  const diff = b-a;
+  if(diff>0){
+    const randNum = a + Math.random() * diff;
+    return parseFloat(randNum.toFixed(accuracy));
+  }else{
+    return Error("getRandomFloat: Ошибка входных данных");
+  }
+}
+
+//getRandomInt(0,100);
+//getRandomFloat(0.5,15.58,2);
