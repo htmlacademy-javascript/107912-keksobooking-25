67c23e42e258f1f9962635bc6967d8bc419e46a5
Dmitrii Suvorov <sdk503@yandex.ru>
2022-03-02 22:31:34 +0700

    add functions for generating objets(4.9)

--- js/main.js
+++ js/main.js
@@ -22,5 +22,58 @@ function getRandomFloat(min, max, accuracy){
   }
 }
 
-getRandomInt(0,100);
-getRandomFloat(0.5,15.58,4);
+const BUILDINGS_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel']; //здания
+const CHECKIN_TIMES = ['12:00','13:00','14:00']; //время заезда
+const CHECKOUT_TIMES = ['12:00','13:00','14:00']; //время выезда
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS_PATH = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg'
+  ,'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg'
+  ,'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+
+const createAuthorObj = ()=>{
+  const nom = getRandomInt(1,10);
+  if(nom===10){
+    return {avatar: `img/avatars/user${nom}.png`};
+  }
+  return {avatar: `img/avatars/user0${nom}.png`};
+};
+
+const createLocationObj = ()=>({lat:getRandomFloat(35.65000,35.70000,5), lng: getRandomFloat(139.70000,139.80000,5)});
+
+const createOfferObj = (lat,lnd)=>{
+  const offer = {};
+  offer.title= 'Лучшее предложение';
+  offer.address= `${lat},${lnd}`;
+  offer.price= getRandomInt(1,10000);
+  offer.type= BUILDINGS_TYPES[getRandomInt(0,BUILDINGS_TYPES.length-1)];
+  offer.rooms= getRandomInt(1,100);
+  offer.guests=  getRandomInt(1,100);
+  offer.checkin= CHECKIN_TIMES[getRandomInt(0,CHECKIN_TIMES.length-1)];
+  offer.checkout= CHECKOUT_TIMES[getRandomInt(0,CHECKOUT_TIMES.length-1)];
+
+  const featuresStart= getRandomInt(0,FEATURES.length-1);
+  const featuresEnd= getRandomInt(featuresStart,FEATURES.length-1);
+  offer.features= FEATURES.slice(featuresStart,featuresEnd+1);
+
+  offer.description= 'Лучшие виды в городе';
+  offer.photos= Array.from({length:getRandomInt(1,100)},()=>PHOTOS_PATH[getRandomInt(0,PHOTOS_PATH.length-1)]);
+
+  return offer;
+};
+
+function CreateAdvert(){
+  this.author = createAuthorObj();
+  this.location = createLocationObj();
+  this.offer = createOfferObj(this.location.lat,this.location.lng);
+}
+
+/*
+const OBJ = new CreateAdvert();
+console.log(OBJ);
+*/
+
+//console.log(Array.from({length:1},()=>new CreateAdvert())); 
+
+Array.from({length:10},()=>new CreateAdvert()); // массив из 10 сгенерированных JS-объектов ()
+
