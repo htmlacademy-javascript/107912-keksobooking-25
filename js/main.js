b10d3181218b301a71cda17eed01663fdfd02d21
Dmitrii Suvorov <sdk503@yandex.ru>
2022-04-12 01:21:17 +0700

    save 22.04.12 01:21

--- js/main.js
+++ js/main.js
@@ -3,6 +3,33 @@ import {createAdvert} from './create-advert.js';
 
 //Array.from({length:10}, createAdvert); // массив из 10 сгенерированных JS-объектов (объявлений)
 
-const advert =createAdvert();
-const map = document.getElementById('map-canvas');
-map.append(generateCard(advert));
+//const advert =createAdvert();
+//const mapCanvas = document.getElementById('map-canvas');
+//map.append(generateCard(advert));
+
+const map = L.map('map-canvas')
+  .setView({
+    lat: 59.92749,
+    lng: 30.31127,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const marker = L.marker(
+  {
+    lat: 59.96831,
+    lng: 30.31748,
+  },
+  {
+    draggable: true,
+  },
+);
+
+marker.addTo(map);
+
+//mapCanvas.append(map);
