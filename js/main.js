66a9582adf3fb17ea828ccdc5336bb0fe1d9a6c3
Dmitrii Suvorov <sdk503@yandex.ru>
2022-04-13 03:22:35 +0700

    Save 13.04.22 3:22

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-// import {generateCard} from './generate-card.js';
-// import {createAdvert} from './create-advert.js';
+import {generateCard} from './generate-card.js';
+import {createAdvert} from './create-advert.js';
+const MAIN_PIN_SIZE = 52;
+const PIN_SIZE = 40;
+
+const mainPinIcon = L.icon({
+  iconUrl: 'img/main-pin.svg',
+  iconSize: [MAIN_PIN_SIZE, MAIN_PIN_SIZE],
+  iconAnchor: [MAIN_PIN_SIZE/2, MAIN_PIN_SIZE],
+});
+
+const pinIcon = L.icon({
+  iconUrl: 'img/pin.svg',
+  iconSize: [PIN_SIZE, PIN_SIZE],
+  iconAnchor: [PIN_SIZE/2, PIN_SIZE],
+});
 
 //Array.from({length:10}, createAdvert); // массив из 10 сгенерированных JS-объектов (объявлений)
 
-//const advert =createAdvert();
-//const mapCanvas = document.getElementById('map-canvas');
-//map.append(generateCard(advert));
+// const advert = createAdvert();
+// const map = document.getElementById('map-canvas');
+// map.append(generateCard(advert));
+
 
 const map = L.map('map-canvas')
   .setView({
@@ -20,12 +35,6 @@ L.tileLayer(
   },
 ).addTo(map);
 
-const mainPinIcon = L.icon({
-  iconUrl: 'img/main-pin.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-});
-
 const marker = L.marker(
   {
     lat: 59.96831,
@@ -37,10 +46,21 @@ const marker = L.marker(
   }
 );
 
+const marker2 = L.marker(
+  {
+    lat: 59.95515,
+    lng: 30.31748,
+  },
+  {
+    draggable: false,
+    icon: pinIcon,
+  }
+);
+//const markersLayer = L.layerGroup().addTo(map);
 
 marker.addTo(map);
+marker2.addTo(map).bindPopup(generateCard(createAdvert()));
 
 marker.on('moveend', (evt) => {
   document.getElementById('address').value = evt.target.getLatLng();
 });
-//mapCanvas.append(map);
